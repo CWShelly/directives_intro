16bad5e443d6825cf471116c4fa1b88867e8ca6f
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-19 13:48:11 -0700

    removed commented out code

--- app/js/entry.js
+++ app/js/entry.js
@@ -3,14 +3,6 @@ const myApp = angular.module('myApp', []);
 
 require('./directives')(myApp);
 
-// myApp.run(['$rootScope', function($rs){
-//     $rs.tasks = [
-//         {desc: 'buy carrots'},
-//         {desc: 'buy potatoes'},
-//         {desc: 'buy vodka'}
-//     ];
-//     this.description = 'Description from controller'
-// }]);
 myApp.controller('TasksController', function(){
     this.tasks = [
         {desc: 'buy carrots'},
